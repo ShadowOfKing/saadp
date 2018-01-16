9a0a8dd5f0039f5c85a0adcacc4d5075d824b93b
ShadowOfKing <ShadowKing65@mail.ru>
2018-01-16 10:52:52 +0500

    add radix sort

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -30,6 +30,24 @@ namespace Lab1
                 ar[i] = mx;
             }
         }
+        public static void Radix(ref int[] ar, int min, int max)
+        {
+            int[] br = new int[ar.Length];
+            Array.Copy(ar, br, ar.Length);
+            int[] cr = new int[max - min + 1];
+            for (var i = 0; i < cr.Length; i++)
+                cr[i] = 0;
+            for (var i = 0; i < ar.Length; i++)
+                cr[ar[i] - min] += 1;
+            for (var i = 1; i < cr.Length; i++)
+                cr[i] += cr[i - 1];
+            for (var i = ar.Length - 1; i >= 0; i--)
+            {
+                br[cr[ar[i] - min] - 1] = ar[i];
+                cr[ar[i] - min] -= 1;
+            }
+            ar = br;
+        }
     }
     class Program
     {
@@ -45,7 +63,28 @@ namespace Lab1
             swatch.Stop();
             var selectTime = swatch.ElapsedMilliseconds.ToString() + "ms";
             Console.WriteLine(String.Format("Selection: {0}", String.Join(",", resArray)));
+            Array.Copy(array, resArray, array.Length);
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            for (int i = 0; i < resArray.Length; i++)
+            {
+                if (resArray[i] > max)
+                {
+                    max = resArray[i];
+                }
+                if (resArray[i] < min)
+                {
+                    min = resArray[i];
+                }
+            }
+            swatch = new System.Diagnostics.Stopwatch();
+            Console.ReadKey();
+            Sort.Radix(ref resArray, min, max);
+            swatch.Stop();
+            var radixTime = swatch.ElapsedMilliseconds.ToString() + "ms";
+            Console.WriteLine(String.Format("Radix: {0}", String.Join(",", resArray)));
             Console.WriteLine(String.Format("Selection time: {0}", selectTime));
+            Console.WriteLine(String.Format("Radix time: {0}", radixTime));
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
